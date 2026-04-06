bcf3cddbb6e1f555a80e9acc96dc76f1a5d69ad3
Blecku <krupski.kam@gmail.com>
2026-04-06 15:58:55 +0200

    Refactoring registration

--- src/QuickServices/DependencyInjection.cs
+++ src/QuickServices/DependencyInjection.cs
@@ -9,48 +9,22 @@ public static class DependencyInjection
     {
         foreach (var type in assembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract))
         {
-            RegisterByAttribute(services, type, typeof(TransientServiceAttribute), typeof(TransientServiceAttribute<>), services.AddTransient);
-            RegisterByAttribute(services, type, typeof(ScopedServiceAttribute), typeof(ScopedServiceAttribute<>), services.AddScoped);
-            RegisterByAttribute(services, type, typeof(SingletonServiceAttribute), typeof(SingletonServiceAttribute<>), services.AddSingleton);
+            foreach (var attr in type.GetCustomAttributes(false))
+            {
+                var serviceType = attr.GetType().IsGenericType ? attr.GetType().GetGenericArguments()[0] : type;
 
-            RegisterKeyedByAttribute(services, type, typeof(KeyedTransientServiceAttribute), typeof(KeyedTransientServiceAttribute<>), services.AddKeyedTransient);
-            RegisterKeyedByAttribute(services, type, typeof(KeyedScopedServiceAttribute), typeof(KeyedScopedServiceAttribute<>), services.AddKeyedScoped);
-            RegisterKeyedByAttribute(services, type, typeof(KeyedSingletonServiceAttribute), typeof(KeyedSingletonServiceAttribute<>), services.AddKeyedSingleton);
+                switch (attr)
+                {
+                    case IKeyedService keyed:
+                        services.Add(new ServiceDescriptor(serviceType, keyed.ServiceKey, type, keyed.Lifetime));
+                        break;
+                    case IService service:
+                        services.Add(new ServiceDescriptor(serviceType, type, service.Lifetime));
+                        break;
+                }
+            }
         }
 
         return services;
-    }
-
-    private static void RegisterKeyedByAttribute(
-        IServiceCollection services,
-        Type type,
-        Type attributeType,
-        Type genericAttributeType,
-        Func<Type, object?, Type, IServiceCollection> register)
-    {
-        foreach (var attr in type.GetCustomAttributes(false)
-            .Where(a => a.GetType() == attributeType ||
-                       (a.GetType().IsGenericType && a.GetType().GetGenericTypeDefinition() == genericAttributeType)))
-        {
-            var serviceType = attr.GetType().IsGenericType ? attr.GetType().GetGenericArguments()[0] : type;
-            var serviceKey = attr.GetType().GetProperty(nameof(IKeyedService.ServiceKey))!.GetValue(attr);
-            register(serviceType, serviceKey, type);
-        }
-    }
-
-    private static void RegisterByAttribute(
-        IServiceCollection services,
-        Type type,
-        Type attributeType,
-        Type genericAttributeType,
-        Func<Type, Type, IServiceCollection> register)
-    {
-        foreach (var attr in type.GetCustomAttributes(false)
-            .Where(a => a.GetType() == attributeType ||
-                       (a.GetType().IsGenericType && a.GetType().GetGenericTypeDefinition() == genericAttributeType)))
-        {
-            var serviceType = attr.GetType().IsGenericType ? attr.GetType().GetGenericArguments()[0] : type;
-            register(serviceType, type);
-        }
     }
 }
