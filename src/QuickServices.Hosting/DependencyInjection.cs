e82a155f0f7623f81c5e96583c01c50e42dd9dd8
Blecku <krupski.kam@gmail.com>
2026-04-06 16:05:35 +0200

    Refactoring registration HostingServices

--- src/QuickServices.Hosting/DependencyInjection.cs
+++ src/QuickServices.Hosting/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using System.Reflection;
 
@@ -18,7 +19,7 @@ public static class DependencyInjection
                 throw new InvalidOperationException(
                     $"Type '{type.FullName}' is marked with [HostedService] but does not implement IHostedService.");
 
-            services.Add(new ServiceDescriptor(typeof(IHostedService), type, ServiceLifetime.Singleton));
+            services.TryAddEnumerable(ServiceDescriptor.Singleton(typeof(IHostedService), type));
         }
 
         return services;
